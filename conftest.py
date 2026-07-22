83dbc327fbcf8506619c081e351bab2548c3f239
Leonid <leonid.ovchinnikov.2000@mail.ru>
2026-07-22 21:46:14 +0800

    123

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture
+def driver():
+
+
+
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+
+    yield driver
+
+    driver.quit()
